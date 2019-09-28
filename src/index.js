afb8da5ebdbfda9ff10a8c4c1cc85c1300942810
John Towery <kbaries@gmail.com>
2019-09-28 16:11:41 -0600

    added babel and etc

--- src/index.js
+++ src/index.js
@@ -1,1 +1,101 @@
-console.log('Hello Project.');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import uuidv4 from 'uuid/v4';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use((req, res, next) => {
+    req.me = users[1];
+    next();
+});
+
+let users = {
+    1: {
+        id: '1',
+        username: 'Robin Wieruch',
+    },
+    2: {
+        id: '2',
+        username: 'Dave Davids',
+    },
+};
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'By World',
+        userId: '2',
+    },
+};
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
+});
+
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+app.post('/users', (req, res) => {
+    return res.send('POST HTTP method on user resource');
+});
+
+app.delete('/users/:userId', (req, res) => {
+    return res.send(
+        `DELETE HTTP method on user/${req.params.userId} resource`,
+    );
+});
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+});
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+});
+
+app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+        id,
+        text: req.body.text,
+    };
+    messages[id] = message;
+    return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+    const {
+        [req.params.messageId]: message,
+        ...otherMessages
+    } = messages;
+    messages = otherMessages;
+    return res.send(message);
+});
+
+
+app.listen(process.env.PORT, () =>
+    console.log('Example app listening on port 3000!'),
+);
